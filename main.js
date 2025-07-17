abadf6284289aa752e1248a4f98632be9142aacf
frex1341 <freedomexplore1341@gmail.com>
2025-07-18 08:33:09 +0900

    main.js を更新

--- main.js
+++ main.js
@@ -83,10 +83,10 @@ function initDB() { // IndexedDBの初期化
 }
 
 function getDaysBetweenDates(dateStr1, dateStr2) {
-  const d1 = new Date(dateStr1 + 'T00:00:00'); // 時刻固定
-  const d2 = new Date(dateStr2 + 'T00:00:00');
-  const msPerDay = 1000 * 60 * 60 * 24;
-  return Math.round((d2 - d1) / msPerDay);
+  const date1 = new Date(dateStr1 + "T00:00:00");
+  const date2 = new Date(dateStr2 + "T00:00:00");
+  const diffTime = date2.getTime() - date1.getTime();
+  return Math.ceil(diffTime / (1000 * 60 * 60 * 24));
 }
 
 //デッキセクション
