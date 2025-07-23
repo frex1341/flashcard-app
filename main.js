649d44031d8dfed0370c8afc53bbfd6534264213
frex1341 <freedomexplore1341@gmail.com>
2025-07-23 10:38:05 +0900

    main.js を更新

--- main.js
+++ main.js
@@ -236,8 +236,8 @@ function renderDeckList(decks) {
 　　　const todayStr = new Date().toISOString().split('T')[0];
 　　　const daysLeft = getDaysBetweenDates(todayStr, deck.nextReviewDate);
     const span = document.createElement("span");
-    const setsuyaku = math.round((1 - deck.currentTimeSecond/deck.maxTimeSecond)*100);
-    span.textContent = `📘 ${deck.name}（あと ${daysLeft} 日）,節約率${setsuyaku}`;
+    const setsuyaku = (1 - deck.currentTimeSecond/deck.maxTimeSecond)*100;
+    span.textContent = `📘 ${deck.name}（あと ${daysLeft} 日）,節約率${setsuyaku|| 0}%}`;
     span.style.marginRight = "10px";
     span.addEventListener("click", () => {
       selectedDeckName = deck.name;
